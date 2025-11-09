f0046f3c4b735f0a70a4d3417cc75eb7ad88d7f5
rimiakter4 <rimia9941@gmail.com>
2025-11-09 08:30:16 +0600

    connected a mongodb

--- index.js
+++ index.js
@@ -2,12 +2,39 @@ const express =require("express")
 const app =express()
 const cors=require("cors")
 const port =process.env.PORT||3000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 app.use(cors())
-app.use(express())
+app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q32hk1x.mongodb.net/?appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+async function run() {
+  try {
+
+    await client.connect();
+  
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
-  res.send('travelids server is running')
+  res.send('travelids server is running on port ')
 })
 
 app.listen(port, () => {
