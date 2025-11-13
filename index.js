96c2d908be466a487ac84c462d562e0e58558df3
rimiakter4 <rimia9941@gmail.com>
2025-11-13 15:52:42 +0600

    added a all vehicles and my vehicles function

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express =require("express")
 const app =express()
 const cors=require("cors")
 const port =process.env.PORT||3000
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
-app.use(cors())
+
+app.use(cors({
+  origin: "http://localhost:5173",  
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
+}));
+
+
 app.use(express.json())
 
 
@@ -24,7 +32,118 @@ async function run() {
   try {
 
     await client.connect();
-  
+    const db=client.db("travelease_db")
+  const Vehiclecollections=db.collection("vehicles")
+     const userscollection=db.collection('users')
+        const bookingsCollection = db.collection("bookings");
+// bokking collection
+
+
+app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result); 
+    });
+    
+
+app.post('/vehicles', async (req, res) => {
+  const newvehicle = req.body;
+  console.log("New Vehicle Received:", newvehicle);
+
+  try {
+    const result = await Vehiclecollections.insertOne(newvehicle);
+    res.send(result);
+  } catch (error) {
+    console.error("Error inserting vehicle:", error);
+    res.status(500).send({ message: "Failed to add vehicle" });
+  }
+});
+
+app.get('/vehicles',async(req,res)=>{
+    const cursor=Vehiclecollections.find().sort({created_at:-1}).limit(6)
+            const result=await cursor.toArray()
+            res.send(result)
+})
+
+app.get('/all-vehicles',async(req,res)=>{
+  const cursor=Vehiclecollections.find().sort({created_at:-1})
+  const result=await cursor.toArray()
+  res.send(result)
+})
+
+
+ app.get('/all-vehicles/:id',async(req,res)=>{
+            const id=req.params.id
+            const qurey={_id : new ObjectId(id)}
+            const result=await Vehiclecollections.findOne(qurey)
+            res.send(result)
+        })
+
+
+
+app.get("/my-vehicles", async (req, res) => {
+  const email = req.query.email; 
+  if (!email) {
+    return res.status(400).send({ message: "Email is required" });
+  }
+
+  const vehicles = await Vehiclecollections.find({ userEmail: email }).toArray();
+  res.send(vehicles);
+});
+
+app.patch('/all-vehicles/:id',async(req,res)=>{
+            const id=req.params.id
+            const newprodct=req.body
+            const qurey={_id : new ObjectId(id)}
+            const update = {
+  $set: {
+    vehicleName: newprodct.vehicleName,
+    owner: newprodct.owner,
+    category: newprodct.category,
+    location: newprodct.location,
+    pricePerDay: newprodct.pricePerDay,
+    availability: newprodct.availability,
+    description: newprodct.description,
+    coverImage: newprodct.coverImage
+  }
+};
+
+    
+            const result=await Vehiclecollections.updateOne(qurey,update)
+            res.send(result)
+});
+
+       
+app.delete('/all-vehicles/:id',async(req,res)=>{
+            const id=req.params.id
+            const qurey={_id: new ObjectId(id)}
+            const result=await Vehiclecollections.deleteOne(qurey)
+            res.send(result)
+        })    
+
+// user api s 
+ app.post('/users',async(req,res)=>{
+            const newuser=req.body
+            const email=newuser.email
+            const qurey={email:email}
+            const existingUser=await userscollection.findOne(qurey)
+            if(existingUser){
+                res.send({message:"user alredy exiatis"})
+
+            }
+            else{
+   const result=await userscollection.insertOne(newuser)
+            res.send(result)
+            }
+         
+        })
+ 
+
+
+
+
+
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
